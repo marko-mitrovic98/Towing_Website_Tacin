2c8e98e6ba114248ebd07b8e4889bfa47fc99ca6
MareCZV <markomitrovic202@gmail.com>
2025-04-23 20:30:29 +0200

    hero new 2

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,23 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import background1 from '../assets/images/backgorund-1.png';
+import background2 from '../assets/images/backgorund-2.png';
+import background3 from '../assets/images/backgorund-3.png';
 
 const slides = [
     {
-        title: "Slep sluzba",
-        heading: "Tacin",
-        text: "Nosivost do 60t",
-        image: "/assets/hero1.png",
+        title: 'Šlep služba',
+        heading: 'Tacin',
+        text: 'Nosivost do 60t',
+        image: '/assets/hero1.png',
+        background: background1,
     },
     {
-        title: "Dostupni",
-        heading: "24/7",
-        text: "Spremni da odgovorimo na poziv bilo kada i bilo gde.",
-        image: "/assets/hero2.png",
+        title: 'Dostupni',
+        heading: '25/7',
+        text: 'Spremni da odgovorimo na poziv bilo kada i bilo gde.',
+        image: '/assets/hero2.png',
+        background: background2,
     },
     {
-        title: "Iskustvo",
-        heading: "Sigurnost",
-        text: "Naš tim profesionalaca obezbeđuje siguran i bezbedan transport vašeg vozila.",
-        image: "/assets/hero3.png",
+        title: 'Iskustvo',
+        heading: 'Sigurnost',
+        text: 'Naš tim profesionalaca obezbeđuje siguran i bezbedan transport vašeg vozila.',
+        image: '/assets/hero3.png',
+        background: background3,
     },
 ];
 
@@ -49,33 +55,37 @@ export const Hero = () => {
         }, 500);
     };
 
-    const { title, heading, text, image } = slides[currentSlide];
+    const { title, heading, text, image, background } = slides[currentSlide];
 
     return (
-        <section className='section-hero'>
-            <div className='container'>
-                <div className='row'>
-                    <div className={`col-6 slide-text ${isAnimating ? "slide-out" : "slide-in"}`}>
+        <section className="section-hero">
+            <div className={`section-hero__background-wrapper ${isAnimating ? 'fade-out' : 'fade-in'}`}>
+                <img src={background} className="section-hero__background" alt="background" />
+            </div>
+
+            <div className="container">
+                <div className="row">
+                    <div className={`col-6 slide-text ${isAnimating ? 'slide-out' : 'slide-in'}`}>
                         <h2>{title}</h2>
                         <h1>{heading}</h1>
                         <p>{text}</p>
-                        <a href='tel:+381694881395' className='btn'>
+                        <a href="tel:+381694881395" className="btn">
                             Pozovite nas!
                         </a>
                     </div>
-                    <div className='col-6'>
-                    <div className={`section-hero__image ${isAnimating ? 'image-slide-out' : 'image-slide-in'}`}>
+                    <div className="col-6">
+                        <div className={`section-hero__image ${isAnimating ? 'image-slide-out' : 'image-slide-in'}`}>
                             <img src={image} alt={heading} />
                         </div>
                     </div>
                 </div>
-                <div className='row'>
-                    <div className='col-12'>
-                        <div className='slider-controls'>
-                            <button className='arrow arrow-left' onClick={handlePrev}>
+                <div className="row">
+                    <div className="col-12">
+                        <div className="slider-controls">
+                            <button className="arrow arrow-left" onClick={handlePrev}>
                                 &larr;
                             </button>
-                            <button className='arrow arrow-right' onClick={handleNext}>
+                            <button className="arrow arrow-right" onClick={handleNext}>
                                 &rarr;
                             </button>
                         </div>
